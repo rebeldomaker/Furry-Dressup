272eebb1ad5b3a0730445e456f16b20d2119d961
Reb <rebeldomaker@icloud.com>
2026-09-22 01:09:37 +0200

    Initial commit

---
+++ DragNDrop.cs
@@ -0,0 +1,33 @@
+using Godot;
+using System;
+
+public partial class DragNDrop : Sprite2D
+{
+	// TODO fix pseudo-code down below:
+	
+	// XXX if the user's cursor is currently dragging clothing item/prop:
+	// if dragging 
+	
+	{
+		// mouse position
+		// get global mouse position, set vector zero for now and FIXME Later
+		position = global 
+	}
+	// create a method here
+	public void inputEvent()
+	{
+		// TODO
+		// if event is inputMouseButton
+			{
+				// if is.pressed
+					{
+						// dragging = True}
+					}
+				// else
+					{
+						dragging = False
+					}
+			
+	}
+	
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# OC-Dressup-Game
+ inspired by 2000s era dressup games, but with FURRIES
