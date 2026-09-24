3b6c62e5070ec6dfbf538b82d7636396865d2755
Reb <rebeldomaker@icloud.com>
2026-09-24 20:31:10 +0200

    reformatted comments for readability?

--- DragNDrop.cs
+++ DragNDrop.cs
@@ -3,13 +3,14 @@ using System;
 
 public partial class DragNDrop : Sprite2D
 { 
-	private bool _isDragging = false;   // if the user's cursor is currently dragging clothing item/prop, defaults to false
-	private Vector2 _dragOffset = Vector2.Zero; // the distance between where you clicked inside the sprite and the sprite's center point.
-// NOTE set vector zero for now, maybe change it later
-
-// override⁠: Tells C# that you are replacing Godot's base ⁠_Process⁠ method (inherited from ⁠Node⁠) with a new custom version.
+	private bool _isDragging = false; 
+	// if the user's cursor is currently dragging clothing item/prop, defaults to false
+	private Vector2 _dragOffset = Vector2.Zero; 
+	// the distance between where you clicked inside the sprite and the sprite's center point.
+	// NOTE set vector zero for now, maybe change it later
 	public override void _Process(double delta)
-	{
+	 {	// override: Tells C# that you are replacing Godot's base_Process method (inherited from Node) with a new custom version.
+
 		// If actively dragging, follow the mouse position (accounting for offset)
 		if (_isDragging)
 		{
