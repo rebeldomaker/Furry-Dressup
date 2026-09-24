42b1f3134fda08c50990c9de59e90496489d805e
Reb <rebeldomaker@icloud.com>
2026-09-24 07:54:11 +0200

    Comments and FIXME added

--- DragNDrop.cs
+++ DragNDrop.cs
@@ -4,7 +4,8 @@ using System;
 public partial class DragNDrop : Sprite2D
 { 
 	private bool _isDragging = false;   // if the user's cursor is currently dragging clothing item/prop, defaults to false
-	private Vector2 _dragOffset = Vector2.Zero;
+	private Vector2 _dragOffset = Vector2.Zero; // the distance between where you clicked inside the sprite and the sprite's center point.
+// NOTE set vector zero for now, maybe change it later
 
 // override⁠: Tells C# that you are replacing Godot's base ⁠_Process⁠ method (inherited from ⁠Node⁠) with a new custom version.
 	public override void _Process(double delta)
@@ -16,9 +17,7 @@ public partial class DragNDrop : Sprite2D
 		}
 	}
 
-	/// <summary>
-	/// Attach this method to the child Area2D's input_event signal in the Godot Inspector.
-	/// </summary>
+	// FIXME Attach this method to the child Area2D's input_event signal in the Godot Inspector.
 	public void _OnArea2DInputEvent(Node viewport, InputEvent @event, long shapeIdx)
 	{
 		if (@event is InputEventMouseButton mouseButtonEvent)
